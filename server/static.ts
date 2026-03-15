cdfb0e4226e9ce66de724d95e49d9efe3d734add
King_Aryan <officialvelorastudio@gmail.com>
2026-03-16 01:18:49 +0530

    6th commit

--- server/static.ts
+++ server/static.ts
@@ -1,9 +1,14 @@
 import express, { type Express } from "express";
 import fs from "fs";
-import path from "path";
+import path, {dirname} from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export function serveStatic(app: Express) {
   const distPath = path.resolve(__dirname, "public");
+
   if (!fs.existsSync(distPath)) {
     throw new Error(
       `Could not find the build directory: ${distPath}, make sure to build the client first`,
